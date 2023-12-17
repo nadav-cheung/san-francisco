e27a394ea3b2aea7be23234634ad89f50b4544b7
nadav <nadav.cheung@gmail.com>
2023-12-17 20:17:03 +0800

    move package names

--- san-francisco-common/src/main/java/cn/com/nadav/sanfrancisco/common/properties/CommonProperties.java
+++ san-francisco-common/src/main/java/cn/com/nadav/sanfrancisco/common/properties/CommonProperties.java
@@ -1,10 +1,10 @@
 package cn.com.nadav.sanfrancisco.common.properties;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "san.francisco.common")
+//@ConfigurationProperties(prefix = "san.francisco.common")
 public class CommonProperties {
 
 
--- san-francisco-web/src/main/java/cn/com/nadav/sanfrancisco/configure/SanFranciscoConfiguration.java
+++ san-francisco-web/src/main/java/cn/com/nadav/sanfrancisco/configure/SanFranciscoConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties({
-        CommonProperties.class,
+//        CommonProperties.class,
         DaoProperties.class,
         ServiceProperties.class,
         TaskProperties.class,
